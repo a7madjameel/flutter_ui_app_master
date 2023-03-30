c1cb05efdc978d17398cbd5de764c866eba0055b
ahmed <ahmoy6651@gmail.com>
2023-03-30 07:22:18 +0300

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ import 'package:ui_app/screens/login_screen.dart';
 import 'package:ui_app/screens/onboarding/onboarding_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
